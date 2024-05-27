53c69b0f32eeddd686d62d0160da5a4f427d835e
Shahathir <shahathiriskandar42@gmail.com>
2024-05-27 18:28:07 +0800

    next 14 update, and adding dockerfile

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   env: {
     RAWG_API_KEY: process.env.RAWG_API_KEY,
   },
   images: {
-    domains: ["cdn.discordapp.com", "media.rawg.io"]
+    domains: ["cdn.discordapp.com", "media.rawg.io", "via.placeholder.com"]
   },
 };
 
